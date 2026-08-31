e2c7d566708b0e82f619e436467de148eace26dd
ayersant-sys <ayer.sant@gmail.com>
2026-08-31 17:40:51 -0600

    Test calibration range flags

--- tests/test-total-biomass.R
+++ tests/test-total-biomass.R
@@ -23,3 +23,25 @@ mixed <- suppressWarnings(frtc_total_biomass(
 stopifnot(mixed$estimation_status[1] == "estimated")
 stopifnot(mixed$estimation_status[2] == "unsupported_species")
 stopifnot(is.na(mixed$frtc_total_biomass_kg[2]))
+
+
+within <- frtc_total_biomass(
+  30, 20, "sal", keep_inputs = TRUE
+)
+stopifnot(isTRUE(within$within_calibration_range))
+stopifnot(within$calibration_status == "within_observed_range")
+stopifnot(within$calibration_dbh_min_cm == 6.7)
+stopifnot(within$calibration_dbh_max_cm == 102.4)
+
+outside <- suppressWarnings(frtc_total_biomass(
+  110, 45, "sal", keep_inputs = TRUE
+))
+stopifnot(identical(outside$within_calibration_range, FALSE))
+stopifnot(outside$calibration_status ==
+            "multiple_dimensions_outside_range")
+
+unsupported <- suppressWarnings(frtc_total_biomass(
+  20, 15, "acacia_catechu", keep_inputs = TRUE
+))
+stopifnot(is.na(unsupported$within_calibration_range))
+stopifnot(unsupported$calibration_status == "not_available")
