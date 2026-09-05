5bb529ff0b36accf8539eb8e7f64f3f4163f49b3
ayersant-sys <ayer.sant@gmail.com>
2026-09-05 12:13:22 -0600

    Test all published FRTC volume coefficients

--- tests/test-frtc-volume.R
+++ tests/test-frtc-volume.R
@@ -1,5 +1,41 @@
 library(nepalallometry)
 
+# FRTC 2025 reference table: selected volume equations reported in Tables 6-8.
+# These tests protect source fidelity. nepalallometry should reproduce the
+# published model forms, coefficients, and sample sizes without alteration.
+expected_models <- data.frame(
+  species_code = rep(c("An", "Cs", "Lp", "Pr", "Sr", "Sw", "Ta"), 3),
+  volume_type = rep(c("total_ob", "ub_20cm", "ub_10cm"), each = 7),
+  form = c(
+    "a_db_hc", "a_d2h_b", "a_d2h_b", "a_d2h_b", "a_d2h_b", "a_db_hc", "a_d2h_b",
+    "a_d2h_b", "a_d2h_b", "linear_d2h", "a_d2h_b", "a_d2h_b", "a_db_hc", "a_d2h_b",
+    "a_db_hc", "a_d2h_b", "a_d2h_b", "a_d2h_b", "a_db_hc", "a_db_hc", "a_d2h_b"
+  ),
+  a = c(
+    0.000048, 0.000064, 0.000064, 0.000058, 0.000059, 0.000047, 0.000070,
+    0.000023, 0.000009, -0.147542, 0.000007, 0.000007, 0.000003, 0.000010,
+    0.000018, 0.000031, 0.000014, 0.000014, 0.000011, 0.000010, 0.000021
+  ),
+  b = c(
+    1.769901, 0.936534, 0.936459, 0.957300, 0.948535, 1.677002, 0.928364,
+    1.016204, 1.089575, 0.000031, 1.121720, 1.113125, 1.994043, 1.074116,
+    1.785698, 0.987658, 1.068129, 1.064049, 1.960877, 1.765752, 1.013348
+  ),
+  c = c(
+    1.165658, NA, NA, NA, NA, 1.254950, NA,
+    NA, NA, NA, NA, NA, 1.644049, NA,
+    1.403011, NA, NA, NA, 1.308190, 1.523197, NA
+  ),
+  sample_size = c(
+    52L, 52L, 46L, 96L, 122L, 47L, 61L,
+    43L, 42L, 31L, 82L, 107L, 39L, 53L,
+    51L, 49L, 44L, 94L, 119L, 46L, 59L
+  ),
+  stringsAsFactors = FALSE
+)
+actual_models <- getFromNamespace(".frtc_volume_models", "nepalallometry")
+stopifnot(isTRUE(all.equal(actual_models, expected_models, check.attributes = FALSE)))
+
 # FRTC 2025 hand calculations for Shorea robusta.
 sal <- frtc_volume(60, 25, "sal", keep_inputs = TRUE)
 stopifnot(isTRUE(all.equal(
@@ -14,8 +50,9 @@ stopifnot(isTRUE(all.equal(
   sal$frtc_volume_ub_10cm_m3,
   0.000011 * 60^1.960877 * 25^1.308190
 )))
-stopifnot(sal$volume_boundary ==
-            "Stem only; 30-cm stump excluded; branches excluded")
+stopifnot(grepl("30-cm stump excluded", sal$volume_boundary, fixed = TRUE))
+stopifnot(grepl("branches excluded", sal$volume_boundary, fixed = TRUE))
+stopifnot(grepl("under-bark stem", sal$volume_boundary, fixed = TRUE))
 
 # Linear FRTC 20-cm-top model for Lagerstroemia parviflora.
 lp <- frtc_volume(40, 20, "lagerstroemia_parviflora")
